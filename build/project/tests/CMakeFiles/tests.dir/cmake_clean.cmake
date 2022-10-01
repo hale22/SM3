b7ad960a0506cbe4a682baaddd3783b811189bc6
Sergey Astapov <hale22dr@gmail.com>
2022-10-01 23:20:37 +0300

    Point Check was removed from CI

---
+++ build/project/tests/CMakeFiles/tests.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tests.dir/src/main_test.cpp.o"
+  "CMakeFiles/tests.dir/src/main_test.cpp.o.d"
+  "CMakeFiles/tests.dir/src/utils.cpp.o"
+  "CMakeFiles/tests.dir/src/utils.cpp.o.d"
+  "tests"
+  "tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
